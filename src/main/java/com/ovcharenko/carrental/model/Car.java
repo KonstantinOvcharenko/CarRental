1a36ccd417eab7151072473320b1c53c7db19113
Konstantin Ovcharenko <konstantin.ovcharenko.95@gmail.com>
2021-10-29 12:35:58 +0300

    Task_1 Added annotations @Column and @Table to all fields and classes, added new fields to RentalOrder class: startDate. endDate, totalPrice, enums made public

--- src/main/java/com/ovcharenko/carrental/model/Car.java
+++ src/main/java/com/ovcharenko/carrental/model/Car.java
@@ -1,20 +1,23 @@
 package com.ovcharenko.carrental.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "CAR")
 public class Car {
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "model")
     private String model;
+
+    @Column(name = "pricePerDay")
     private BigDecimal pricePerDay;
 
-    private enum Color {
+    public enum Color {
         BLACK, WHITE, GREY, RED, GREEN, BLUE, YELLOW, ORANGE;
     }
 }
--- src/main/java/com/ovcharenko/carrental/model/User.java
+++ src/main/java/com/ovcharenko/carrental/model/User.java
@@ -1,15 +1,18 @@
 package com.ovcharenko.carrental.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "USER")
 public class User {
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
 }
